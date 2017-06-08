13730dd2460acbaa8a6ea8d60cf44962091c7ab5
钟晨宇 <zhongchenyu_89@163.com>
2017-06-09 03:30:45 +0800

    modify phpinfo page time

--- app/Http/Controllers/JokeController.php
+++ app/Http/Controllers/JokeController.php
@@ -113,7 +113,7 @@ class JokeController extends BaseController {
   }
 
   public function info() {
-    echo date('Y-m-d H:i:s', time()). '\n';
+    echo date('Y-m-d H:i:s'). '<br>';
     echo phpinfo();
   }
 }
