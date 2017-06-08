04ccac7f40a9e4f72bb192eee2b686759f176bdf
钟晨宇 <zhongchenyu_89@163.com>
2017-06-09 02:49:14 +0800

    use storge

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -1,9 +1,7 @@
-<?php
-
-namespace App\Console\Commands;
+<?php namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-
+use Illuminate\Support\Facades\Storage;
 class Test extends Command
 {
     /**
