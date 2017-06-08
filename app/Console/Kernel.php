959f24e9398dd6cc450a3c5c1eece9a668258440
钟晨宇 <zhongchenyu_89@163.com>
2017-06-08 15:17:38 +0800

    add scheduel spider

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,38 +5,39 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-      Commands\MultithreadingRequest::class,
-      Commands\JokeSpider::class,
-      Commands\ImageSpider::class,
-    ];
+class Kernel extends ConsoleKernel {
+  /**
+   * The Artisan commands provided by your application.
+   *
+   * @var array
+   */
+  protected $commands = [
+    Commands\MultithreadingRequest::class,
+    Commands\JokeSpider::class,
+    Commands\ImageSpider::class,
+  ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+  /**
+   * Define the application's command schedule.
+   *
+   * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+   * @return void
+   */
+  protected function schedule(Schedule $schedule)
+  {
+    $schedule->command('spider:joke')
+      ->dailyAt('1:00:00');
+    $schedule->command('spider:image')
+      ->dailyAt('1:00:00');
+  }
 
-    /**
-     * Register the Closure based commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        require base_path('routes/console.php');
-    }
+  /**
+   * Register the Closure based commands for the application.
+   *
+   * @return void
+   */
+  protected function commands()
+  {
+    require base_path('routes/console.php');
+  }
 }
